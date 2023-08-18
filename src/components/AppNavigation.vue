c44d1594d4b1cf8282668504a9b4469a76ad94c1
wechu07 <wechu017@gmail.com>
2023-08-18 17:11:35 +0300

    add app navigation

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="sticky top-0 bg-secondary shadow-lg">
+    <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-cloud text-2xl"></i>
+          <p>Weather</p>
+        </div>
+      </RouterLink>
+
+      <div class="flex gap-3 flex-1 justify-end">
+        <i @click="toggleModal"
+          class="fa-solid fa-circle-question text-xl hover:text-primary duration-150 cursor-pointer"></i>
+        <i class="fa-solid fa-plus text-xl hover:text-primary duration-150 cursor-pointer"></i>
+      </div>
+      <InformationModal :modalActive="modalActive" @close-modal="toggleModal">
+        <h1 class="text-black">Hey there</h1>
+        <p class="text-black">I will add more things later</p>
+      </InformationModal>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+import InformationModal from "./InformationModal.vue";
+import { ref } from "vue";
+
+const modalActive = ref(null);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const mapboxAPIKey = import.meta.env.VITE_MAPBOX_API_KEY;
+
+const router = useRouter();
+
+const previewCity = (searchResult) => {
+  console.log(searchResult);
+  const [city, region] = searchResult.place_name.split(",");
+  console.log(city, region);
+  router.push({
+    name: "cityView",
+    params: {
+      city: city.replaceAll(" ", "")
+    },
+    query: {
+      // for example
+      /**
+       * {
+      "type": "Point",
+      "coordinates": [
+                36.438783,
+                -0.722442
+              ]
+        }
+       */
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true // just previewing
+    }
+  })
+}
+
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const searchError = ref(false);
+const mapboxSearchResults = ref(null);
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      try {
+        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`)
+        mapboxSearchResults.value = result.data.features;
+        console.log(mapboxSearchResults.value)
+      } catch {
+        searchError.value = true;
+      }
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 300)
+}
+</script>
+
+<template>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input v-model="searchQuery" @input="getSearchResults" type="text" placeholder="Search for a city or region"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-primary focus:outline-none focus:shadow-[0px_1px_0_0_#588157]">
+      <ul class="absolute bg-primary text-white w-full shadow-md py-2 px-1 top-[66px]" v-if="mapboxSearchResults">
+        <p class="py-2" v-if="searchError">We probably need to try again, something went wrong</p>
+        <p class="py-2" v-if="!searchError && mapboxSearchResults.length === 0">No results match your query, try a
+          different place</p>
+        <template v-else>
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)">
+            {{ `We have ${searchResult.text} or more accurately, ${searchResult.place_name}` }}
+          </li>
+        </template>
+      </ul>
+    </div>
+  </main>
+</template>
