1e37c891ca72fd3b7a2842ece681e3569a1f8134
wechu07 <wechu017@gmail.com>
2023-08-21 12:38:03 +0300

    update hourly weather forecast

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -6,75 +6,111 @@ const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
 const route = useRoute();
 const getWeatherData = async () => {
-    try {
-        const weatherData = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${route.query.lat},${route.query.lng}&aqi=no`);
+  try {
+    const forecastWeatherData = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${route.query.lat},${route.query.lng}&days=1&aqi=no&alerts=no`);
 
-        // // calculate current date & time
-        // const localOffset = new Date().getTimezoneOffset() * 60000;
-        // const utc = weatherData.data.current.dt * 1000 + localOffset;
-        // weatherData.data.currentTime =
-        //     utc + 1000 * weatherData.data.timezone_offset;
+    await new Promise((res) => setTimeout(res, 1000));
 
-        // // calculate hourly weather offset
-        // weatherData.data.hourly.forEach((hour) => {
-        //     const utc = hour.dt * 1000 + localOffset;
-        //     hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
-        // });
+    const locationName = await forecastWeatherData.data.location.name;
+    const localTime = await forecastWeatherData.data.location.localtime;
+    const currentTemp = await forecastWeatherData.data.current.temp_c;
+    const icon = await forecastWeatherData.data.current.condition.icon;
+    const forecast = await forecastWeatherData.data.forecast.forecastday[0];
+    const hourlyForecast = await forecast.hour;
 
-        await new Promise((res) => setTimeout(res, 1000));
+    // The current UTC timestamp
+    const nowUtcTimestamp = new Date().getTime();
 
-        const locationName = weatherData.data.location.name;
-        const conditionText = weatherData.data.current.condition.text;
-        const currentTemp = weatherData.data.current.temp_c;
-        const localTime = weatherData.data.location.localtime;
+    // Filter the hourly forecast to get only future hours
+    const futureHourlyForecast = hourlyForecast.filter(hour => {
+      const hourUtcTimestamp = new Date(hour.time_epoch * 1000).getTime();
+      return hourUtcTimestamp > nowUtcTimestamp;
+    });
 
-        return {
-            locationName: locationName,
-            conditionText: conditionText,
-            currentTemp: currentTemp,
-            localTime: localTime,
-        };
-    } catch (err) {
-        console.log(err)
-    }
+    const parsedTime = new Date(localTime).toLocaleTimeString();
+
+    return {
+      locationName: locationName,
+      currentTemp: currentTemp,
+      localTime: parsedTime,
+      icon: icon,
+      forecast: forecast,
+      hourlyForecast: futureHourlyForecast,
+    };
+  } catch (err) {
+    console.log(err)
+  }
 }
-const { locationName, conditionText, currentTemp, localTime } = await getWeatherData();
-console.log(locationName, conditionText, currentTemp, localTime)
+const { locationName, currentTemp, localTime, icon, forecast, hourlyForecast } = await getWeatherData();
+console.log(hourlyForecast);
 </script>
 
 <template>
-    <div class="flex flex-col flex-1 items-center">
-        <!-- the banner -->
-        <div v-if="route.query.preview" class="text-white p-4 bg-primary w-full text-center">
-            <p>
-                You are currently previewing this city, click the "+" icon to start
-                tracking this city Currently, it is still not working.
-            </p>
-        </div>
-        <!-- Weather Overview -->
+  <div class="flex flex-col flex-1 items-center">
+    <!-- the banner -->
+    <div v-if="route.query.preview" class="text-white p-4 bg-primary w-full text-center">
+      <p>
+        You are currently previewing {{ locationName }}, click the "+" icon to start
+        tracking this city.
+      </p>
+    </div>
+    <!-- Weather Overview -->
     <div class="flex flex-col items-center text-white py-12">
       <h1 class="text-4xl mb-12">{{ route.params.city }}</h1>
-      <p class="text-sm mb-12"> Where the time is
+      <p class="text-sm mb-12">
         {{
           localTime
         }}
       </p>
-      <p class="text-8xl mb-8"> It is
+      <p class="text-8xl mb-8">
         {{ currentTemp }}&deg;C
       </p>
-      <p>I will add more detail on hourly forecast, weekly and all</p>
-      <!-- <p>
-        Feels like
-        {{ Math.round(weatherData.current.feels_like) }} &deg;
-      </p>
-      <p class="capitalize">
-        {{ weatherData.current.weather[0].description }}
-      </p> -->
-      <!-- <img
-        class="w-[150px] h-auto"
-        :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`"
-        alt=""
-      /> -->
+      <img class="w-[150px] h-auto" :src="`http:${icon}`" alt="" />
     </div>
+
+    <hr class="border-white border-opacity-10 border w-full" />
+
+    <!-- Hourly Weather -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">Hourly Weather</h2>
+        <div class="flex gap-10 overflow-x-scroll">
+          <div v-for="hour in hourlyForecast" :key="hour.time_epoch" class="flex flex-col gap-4 items-center">
+            <p class="whitespace-nowrap text-md">
+              {{ new Date(hour.time).toLocaleTimeString("en-us", {
+                hour: "numeric",
+              }) }}
+            </p>
+            <img class="w-auto h-[50px] object-cover"
+              :src="`http:${hour.condition.icon}`" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
+
+
+    <hr class="border-white border-opacity-10 border w-full" />
+
+    <!-- Weekly Weather -->
+    <!-- <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">7 Day Forecast</h2>
+        <div v-for="day in weatherData.daily" :key="day.dt" class="flex items-center">
+          <p class="flex-1">
+            {{
+              new Date(day.dt * 1000).toLocaleDateString("en-us", {
+                weekday: "long",
+              })
+            }}
+          </p>
+          <img class="w-[50px] h-[50px] object-cover"
+            :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`" alt="" />
+          <div class="flex gap-2 flex-1 justify-end">
+            <p>H: {{ Math.round(day.temp.max) }}</p>
+            <p>L: {{ Math.round(day.temp.min) }}</p>
+          </div>
+        </div>
+      </div>
+    </div> -->
+  </div>
 </template>
